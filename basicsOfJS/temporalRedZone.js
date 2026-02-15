93ee0aa342cd4b108ad9cb86f517dfd24738c2cc
siddhantKumar07 <siddhantk80044@gmail.com>
2026-02-15 23:00:13 +0530

    temporal dead zone

---
+++ basicsOfJS/temporalRedZone.js
@@ -0,0 +1,8 @@
+// temporal dead zone
+
+// temporal dead zone is the time between the start of the block and the point where the variable is declared. During this time, the variable is not accessible and will throw a ReferenceError if you try to access it.
+console.log(name); // This will throw a ReferenceError   because name is in the temporal dead zone
+let name = "John";
+console.log(name); // Output: John, because name is now declared and can be accessed 
+
+//in short tdz means it will not throw error even if the variable is not declare and initialiize , it throw the reference error which is like you cannot access name before initialization.
