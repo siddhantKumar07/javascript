bd699771588bc5ab26bbec8418e8bf7d48aaeb25
siddhantKumar07 <siddhantk80044@gmail.com>
2026-02-17 22:00:30 +0530

    learning the concept of logical operator

---
+++ operators/logicalOperator.js
@@ -0,0 +1,15 @@
+//logical operators
+
+// Logical operators are used to combine or invert boolean values. In JavaScript, there are three logical operators: AND (&&), OR (||), and NOT (!). 
+
+// 1. AND (&&): This operator returns true if both operands are true, and false otherwise. For example:
+let a = true;
+let b = false;
+console.log(a && b); // Output: false, because the AND operator returns false if either operand is false.
+
+// 2. OR (||): This operator returns true if at least one of the operands is true, and false otherwise. For example:
+console.log(a || b); // Output: true, because the OR operator returns true if at least one operand is true.
+
+// 3. NOT (!): This operator returns true if the operand is false, and false if the operand is true. For example:
+console.log(!a); // Output: false, because the NOT operator negates the value of a, which is true, resulting in false.
+console.log(!b); // Output: true, because the NOT operator negates the value of b, which is false, resulting in true.
