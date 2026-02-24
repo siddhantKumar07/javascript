2b6512551eba7d90da52c9c7efb43cc9a3195c17
siddhantKumar07 <siddhantk80044@gmail.com>
2026-02-24 17:41:23 +0530

    input

---
+++ arrays/forEach.js
@@ -0,0 +1,4 @@
+let arr =[4,5,5,5,6,6,6,33,2]
+arr.forEach(function(value){
+    console.log(value)
+});
---
+++ arrays/toGetInut.js
@@ -0,0 +1,2 @@
+let input = prompt("Enter something:");
+console.log("You entered:", input);
