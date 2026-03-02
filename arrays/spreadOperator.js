88289e0082eda49e41b767e1e2f536ae6e12328c
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-02 23:28:49 +0530

    learning spread

---
+++ arrays/spreadOperator.js
@@ -0,0 +1,4 @@
+// Spread operator (...) JavaScript ka ek feature hai jo array ya object ki values ko expand (separate) kar deta hai.
+let arr=[4,2,2,2,2,3,3,4]
+let arr2=[...arr]
+console.log(arr2)
