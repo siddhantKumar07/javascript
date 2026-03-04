c75717bd9c1ae3602c1c7634664167c484da00c4
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 11:12:35 +0530

    doing some qus of every()

---
+++ arrays/everyQus.js
@@ -0,0 +1,5 @@
+//The .every() method in JavaScript is used with arrays to check if all elements satisfy a given condition.
+// It returns true or false.
+
+// 👉 If every element passes the test → true
+// 👉 If any element fails → false
