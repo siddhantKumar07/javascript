1a36c9bc8d64b4fa7ec85aa6eadbc53543ee15c5
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 11:01:06 +0530

    doing some qus of find()

--- arrays/findQus.js
+++ arrays/findQus.js
@@ -1,4 +1,4 @@
 // find the number into an array
 let aroy =[1,2,4,4,55,12,58,76]
-let ans =aroy.find((val)=>{return val>50}  ) 
+let ans =aroy.find((val)=>{return val>50}  ) // it returns the first value which it find like greater than 50 is 55,58,76 but the ans if 55 because it find it find 
 console.log(ans)
