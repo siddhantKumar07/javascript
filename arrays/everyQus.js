9bd7c582e0ebb48777479446c2480df603c20c57
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 13:47:55 +0530

    sollving quus with every()

--- arrays/everyQus.js
+++ arrays/everyQus.js
@@ -1,5 +1,11 @@
 //The .every() method in JavaScript is used with arrays to check if all elements satisfy a given condition.
 // It returns true or false.
 
-// 👉 If every element passes the test → true
-// 👉 If any element fails → false
+// If every element passes the test → true
+// If any element fails → false
+let array=[1,2,3,4,5,6,7,8,9,10]
+ let even =array.every((val)=>{
+    return val%2==0
+ })
+
+ console.log(even)
