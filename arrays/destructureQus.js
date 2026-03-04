d23fff4eb7f85c736f1b0a6e26c08909b70daede
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 13:53:04 +0530

    sollving quus with destructure()

---
+++ arrays/destructureQus.js
@@ -0,0 +1,5 @@
+// destruct the array and give their element a name which is first and second name ;
+let name =["siddhant","kumar"]
+let[firstName,lastName]=name;
+console.log(firstName);
+console.log(lastName)
