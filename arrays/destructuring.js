3cf869f564f261b94b9bb3162b0331d88747ccae
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 13:56:03 +0530

    updating this file

--- arrays/destructuring.js
+++ arrays/destructuring.js
@@ -3,3 +3,4 @@ let [a,b,,c]=arr;
 console.log(a)
 console.log(b)
 console.log(c)
+// Destructuring in JavaScript is a syntax that allows you to unpack values from arrays or properties from objects into separate variables.
