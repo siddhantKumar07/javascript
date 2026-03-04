81bdd70507f047cfeeaf24f81a352209d0c8758d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 11:06:27 +0530

    doing some qus of some()

--- arrays/someQus.js
+++ arrays/someQus.js
@@ -1,6 +1,8 @@
 // use .some() to check if any score less than 40
+//The .some() method in JavaScript is used with arrays to check if at least one element in the array satisfies a given condition.
+// It returns true or false.
 let nums=[20,45,77,85,65,25]
 let gretnum=nums.some((val)=>{
-    return val>55;
+    return val>55;//
 })
 console.log(gretnum)
