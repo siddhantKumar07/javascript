42fc2b8fe539ca1b4f4c3af19b303a84230cab6f
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 10:56:00 +0530

    doing some qus of reduce()

--- arrays/reduceQus.js
+++ arrays/reduceQus.js
@@ -3,5 +3,5 @@
 let array =[1,2,3,4,5,6,7,8,9,10]
 let sum=array.reduce((acc,curr)=>{//it takes two value 1st is previous and secnd is the next value
     return acc+curr
-})
+},0)
 console.log(`sum of all the array is ${sum}`);
