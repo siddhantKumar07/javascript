cb8f746103bdd345fa3f038bbc570cd7a44edd51
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-04 15:10:05 +0530

    finding the smallest  element in the arrray

--- arrays/qus/qus4.js
+++ arrays/qus/qus4.js
@@ -3,3 +3,14 @@
 let arr=[1,2,3,4,5,6,7,8,9]
 let ans =arr.includes(77)
 console.log(ans)
+
+// using different way to find 
+let anss;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==700)
+    {
+        anss=true
+    }
+    else anss =false
+}
+console.log(anss)
