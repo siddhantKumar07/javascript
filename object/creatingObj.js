f298c204ebf44dda9139d5d842090ad6b9d38bb0
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-05 23:24:24 +0530

    updating my first object file

--- object/creatingObj.js
+++ object/creatingObj.js
@@ -1,3 +1,4 @@
+// an Object is a data structure used to store multiple values in the form of key–value pairs. It helps group related data and functions together.
 let data={
     name:"siddhant kumar",
     id:1,
