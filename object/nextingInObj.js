30544248e3fc6c388a498cface243e617044a70e
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-06 11:42:58 +0530

    learning the concept of nested object

---
+++ object/nextingInObj.js
@@ -0,0 +1,13 @@
+
+let details={
+    name:"siddhant",
+    age:22,
+    address:{
+        city:"gorakhpur",
+        state:"up",
+        location:{
+            latitude:55.57,
+            longitude:25.14,
+        }
+    }
+}
