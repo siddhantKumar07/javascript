f621411ec75eefe5a7bef162dc6c2bb8b386fec6
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-07 00:25:35 +0530

    doing some qus

---
+++ object/qus4.js
@@ -0,0 +1,7 @@
+// create a dynamic key let key ="age", how will you acess user[key]
+let key ="age";
+let values={
+    name:"something",
+    age:22
+}
+console.log(values[key])
---
+++ object/qus5.js
@@ -0,0 +1,6 @@
+// destructure the key "first-name" as a variable call firstName;4
+let obji={
+    "first-name":"siddhant"
+}
+let {"first-name":firstName} =obji
+console.log(firstName)
