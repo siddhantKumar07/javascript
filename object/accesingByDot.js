8c4c02040aae0032f10359f8dc622c58948e5722
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-06 11:37:26 +0530

    adding some commemnts

--- object/accesingByDot.js
+++ object/accesingByDot.js
@@ -5,6 +5,6 @@ let student={
     console.log("hello ji iam method which is inside the object ")
     }
 }
-console.log(student.name)
+console.log(student.name)//with this you can go inside the object and access the desired value 
 console.log(student.age)
 console.log(student.hello())
