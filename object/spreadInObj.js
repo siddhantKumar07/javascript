0e6417ced5c05c20e2c68f573deb7dfa553a80c3
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-06 23:08:13 +0530

    deep copy

--- object/spreadInObj.js
+++ object/spreadInObj.js
@@ -38,3 +38,14 @@ obj4.address.city = "Delhi";// it changes the value of obj3 also because nested 
 obj4.name="kaho";// it does not affect the name of obj3
 console.log(obj3.name)
 console.log(obj3.address.city); // Delhi
+
+// deep copy 
+const object = {
+  name: "Rahul",
+  address: {
+    city: "Lucknow"
+  }
+};
+const objs = JSON.parse(JSON.stringify(object));// it create a fully new object 
+objs.address.city = "Delhi";//it does not modify the old one 
+console.log(object.address.city);
