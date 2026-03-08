8e3504dacf9374461016028bab023b3aa43d8dff
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-08 20:52:15 +0530

    starting to learn Document object model

--- Dom/definition.js
+++ Dom/definition.js
@@ -2,3 +2,25 @@
 
 // In simple words:
 //  DOM allows JavaScript to interact with and manipulate the content, structure, and style of a webpage.
+
+/*
+1. How the DOM Works
+
+When a web page loads in the browser:
+
+The browser reads the HTML document.
+
+It converts the HTML into a tree-like structure of objects.
+
+Each HTML element becomes a node (object) in the DOM tree.
+
+JavaScript can then use the DOM to:
+
+Change text
+
+Modify styles
+
+Add or remove elements
+
+Handle events (clicks, typing, etc.)
+*/
