4d4099a4daa5270353afa4d4088c090537f7be82
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-08 20:51:20 +0530

    starting to learn Document object model

--- Dom/definition.js
+++ Dom/definition.js
@@ -1,2 +1,4 @@
 // The Document Object Model (DOM) is a programming interface for web documents (HTML or XML). It represents the structure of a webpage as a tree of objects so that programming languages like JavaScript can access, modify, add, or delete elements dynamically.
 
+// In simple words:
+//  DOM allows JavaScript to interact with and manipulate the content, structure, and style of a webpage.
