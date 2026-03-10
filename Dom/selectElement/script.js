57c144c8c44d1325dcfbe2356b2892fc083ce28a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-10 23:41:37 +0530

    learneed the concept of query selector

--- Dom/selectElement/script.js
+++ Dom/selectElement/script.js
@@ -8,3 +8,11 @@ paragraph.style.fontSize="50px"
 let head=document.getElementsByClassName("iclass")[0]// have to use indexin it
 //getElementsByClassName() returns a collection (list of elements), not a single element. So you cannot directly use textContent on it. 
 head.textContent="its updated by js dom and it is h2"
+
+//using querySelector to select the tag 
+// qurey selector is used to select first matched tag 
+let h1 =document.querySelector('h1')
+h1.innerText="this is slected by the query slector"
+
+// there is another one which is used to select the tag which is querySlectorAll;
+// it selects all the tags which matched to the id or class or tagName
