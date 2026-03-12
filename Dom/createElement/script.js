1e7c62b812be9ce4053184e5cbf9293d4ac4af73
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-12 15:17:41 +0530

    learning the concept of append and prepend

--- Dom/createElement/script.js
+++ Dom/createElement/script.js
@@ -1,4 +1,17 @@
 // this is the way to create a element with the help of js 
 let h1 = document.createElement("h1")
 h1.textContent="helloooooo"
-document.body.appendChild(h1)
+document.body.append(h1)//append() adds content at the end of a parent element.
+
+// prepend() adds content at the beginning of a parent element.
+let h2 =document.createElement("h2")
+h2.innerHTML="hello i am h2"
+document.body.prepend(h2)
+/*
+example of how booth works 
+
+    <h2>hello i am h2</h2>
+    <script src="script.js"></script>
+    <h1>helloooooo</h1>
+
+*/
