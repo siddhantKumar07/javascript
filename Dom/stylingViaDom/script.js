007ad557e3012b06862ea14acf953cd6d840a93f
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-12 15:41:41 +0530

    how to change the style using js

---
+++ Dom/stylingViaDom/script.js
@@ -0,0 +1,11 @@
+let div = document.querySelector("div")
+let para = document.createElement("p")
+para.innerHTML = "heloo i am a para"
+div.append(para);
+para.style.color="blue"
+para.style.fontSize="40px"
+para.style.fontWeight="bold"
+div.style.height="300px"
+div.style.width="300px"
+div.style.border="2px solid black"
+div.style.backgroundColor="lightblue"
