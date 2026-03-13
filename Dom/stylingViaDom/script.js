82bea2c654b607c95d7f802fed2010389feae0e3
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-13 14:44:15 +0530

    doing some manipulation to learn dom

--- Dom/stylingViaDom/script.js
+++ Dom/stylingViaDom/script.js
@@ -17,6 +17,15 @@ console.dir(div)
 // how to add a class to a particular tag 
 
 let h1 = document.querySelector("h1")
-h1.classList.add("hey")
+h1.classList.add("hey")// this is used to add a class 
+h1.classList.remove("hey")// this  is used to remove the class
+/*
+classList.toggle() is used to add or remove a CSS class from an HTML element.
+
+If the class already exists, it removes it.
+
+If the class does not exist, it adds it.
+*/
+h1.classList.toggle("hey")
 
 console.dir(h1)
