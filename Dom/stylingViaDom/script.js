728d4b70843846ca3de0c3f2ea0ece937708dffa
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-13 14:26:38 +0530

    doing some manipulation to learn dom

--- Dom/stylingViaDom/script.js
+++ Dom/stylingViaDom/script.js
@@ -1,12 +1,14 @@
 let div = document.querySelector("div")
 let para = document.createElement("p")
-para.innerHTML = "heloo i am a para"
+para.innerHTML = "heloo i am a paragraph"
 div.append(para);
 para.style.color="blue"
 para.style.fontSize="40px"
 para.style.fontWeight="bold"
+para.style.textTransform="capitalize"
 div.style.height="300px"
 div.style.width="300px"
 div.style.border="2px solid black"
 div.style.backgroundColor="lightblue"
 console.dir(para)
+console.dir(div)
