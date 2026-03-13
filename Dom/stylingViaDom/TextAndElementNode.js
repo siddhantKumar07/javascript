111900c0584a80b9d3a831a497d5e93394bfe618
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-13 14:56:09 +0530

    difference between element node and text node

---
+++ Dom/stylingViaDom/TextAndElementNode.js
@@ -0,0 +1,25 @@
+/*
+1. An Element Node represents an HTML tag.
+Examples of element nodes:
+<html>
+<body>
+<div>
+<p>
+<h1>
+
+<p>Hello World</p>
+
+<p> is an element node.
+
+
+
+*/
+/*
+2. Text Node
+A Text Node represents the text inside an HTML element.
+<p>Hello World</p>
+
+here =>
+    <p> → Element Node
+    Hello World → Text Node
+*/
