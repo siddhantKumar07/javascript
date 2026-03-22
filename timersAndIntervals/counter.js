e20f7876d326ad595bd15e3661171cea58d9920a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-22 14:36:26 +0530

    creating a counter

--- timersAndIntervals/counter.js
+++ timersAndIntervals/counter.js
@@ -1,3 +1,5 @@
+// Write a program that counts down from 10 to 0, printing each number to the console every second. When the count reaches 0, it should print "Liftoff!" and stop the timer.
+
 let count =10;
 let co=setInterval(()=>{
       console.log(count);
