f5390ca0eea2fbd423f95fa1ae7c73dfc4fc0832
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-24 23:38:39 +0530

    learning the storage limit

--- localAndSessionStorageAndCookies/sessionStorage/sessionStorage.js
+++ localAndSessionStorageAndCookies/sessionStorage/sessionStorage.js
@@ -1,5 +1,7 @@
 // session storage is a web storage object that allows you to store key-value pairs in the browser. It is similar to local storage, but the data stored in session storage is only available for the duration of the page session. This means that the data will be lost when the user closes the browser or navigates away from the page.
 
+//storage limit (usually around 5MB) and should not be used to store large amounts of data. Additionally, session storage is specific to the domain and cannot be accessed by other websites.
+
 // To use session storage, you can use the following methods:
 // setItem(key, value): This method is used to store a key-value pair in session storage. The key is a string that identifies the data, and the value can be any data type (string, number, object, etc.). For example:
 sessionStorage.setItem('name', 'John Doe');
