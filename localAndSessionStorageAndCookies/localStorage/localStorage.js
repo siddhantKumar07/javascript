0221fc789642a0abf14ab998c663b948733b55dd
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-24 23:33:54 +0530

    learning the storage limit

--- localAndSessionStorageAndCookies/localStorage/localStorage.js
+++ localAndSessionStorageAndCookies/localStorage/localStorage.js
@@ -1,5 +1,7 @@
 // local storage is a web storage object that allows you to store key-value pairs in the browser. It is similar to session storage, but the data stored in local storage persists even after the browser is closed. This means that the data will be available the next time the user visits the website.
 
+//storage limit (usually around 5MB) and should not be used to store large amounts of data. Additionally, local storage is specific to the domain and cannot be accessed by other websites.
+
 // To use local storage, you can use the following methods:
 // setItem(key, value): This method is used to store a key-value pair in local storage. The key is a string that identifies the data, and the value can be any data type (string, number, object, etc.). For example:
 localStorage.setItem('name', 'John Doe');
