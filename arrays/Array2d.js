d24e0b35ba1728bc58101392c849dfe1893af780
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-25 20:47:13 +0530

    learning 2d array

--- arrays/Array2d.js
+++ arrays/Array2d.js
@@ -1,13 +1,20 @@
-let arr=[
-    [1,2,3],
-    [4,5,6],
-]
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
 
-let arr2=[
-    [1,2,3],
-    [4,5,6],
-]
-let arr3=[...arr2,...arr2]
-console.log(arr3)
+let arr2 = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
 
+let arr3 = [];
 
+for (let i = 0; i < arr.length; i++) {
+  arr3[i] = []; // ✅ create row first
+  for (let j = 0; j < arr[i].length; j++) {
+    arr3[i][j] = arr[i][j] * arr2[i][j];
+  }
+}
+
+console.log(arr3);
