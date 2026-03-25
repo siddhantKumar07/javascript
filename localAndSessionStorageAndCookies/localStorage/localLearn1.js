c4d1a4716a041f42a05dab11d1c82dcd7e59eeda
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-25 21:00:33 +0530

    learning local storage

--- localAndSessionStorageAndCookies/localStorage/localLearn1.js
+++ localAndSessionStorageAndCookies/localStorage/localLearn1.js
@@ -1,3 +1,13 @@
 localStorage.setItem('name', 'Siddhant kumar');
 // const name = localStorage.getItem('name');
 // console.log(name); // Output: Siddhant kumar
+
+// to store the array and obj in local storage we need to convert them into string using JSON.stringify() and when we want to retrieve the original data type we need to use JSON.parse() to convert the string back to its original data type.
+const user = {
+    name: 'Siddhant kumar',
+    age: 22,
+    city: 'Delhi'
+}
+localStorage.setItem('user', JSON.stringify(user));
+const userData = localStorage.getItem('user');
+const userObj = JSON.parse(userData);
