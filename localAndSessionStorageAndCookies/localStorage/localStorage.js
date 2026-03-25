abde38c450206f66c4bd8efcbc8140dacc59d1da
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-25 20:51:23 +0530

    learning local storage

--- localAndSessionStorageAndCookies/localStorage/localStorage.js
+++ localAndSessionStorageAndCookies/localStorage/localStorage.js
@@ -1,5 +1,7 @@
 // local storage is a web storage object that allows you to store key-value pairs in the browser. It is similar to session storage, but the data stored in local storage persists even after the browser is closed. This means that the data will be available the next time the user visits the website.
 
+// you can only store strings in local storage. If you want to store other data types (like objects or arrays), you need to convert them to strings using JSON.stringify() before storing them, and then use JSON.parse() to retrieve the original data type when you get the item from local storage.
+
 //storage limit (usually around 5MB) and should not be used to store large amounts of data. Additionally, local storage is specific to the domain and cannot be accessed by other websites.
 
 // To use local storage, you can use the following methods:
