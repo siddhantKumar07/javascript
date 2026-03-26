85b8bd69e199732df0d04cc29752e39f8d82a9f9
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-26 23:47:11 +0530

    creating a project just add functionality

---
+++ projects/callProject/script.js
@@ -0,0 +1,5 @@
+let add = document.querySelector("#add");
+add.addEventListener("click", function() {
+    let formContainer = document.querySelector(".form-container");
+    formContainer.style.display = "block";
+});
