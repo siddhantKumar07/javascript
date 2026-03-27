81fcbfdd9725797d74346e7d4e7d624761ee68f1
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-27 19:11:37 +0530

    learning the concept of execution context

---
+++ scope/ExecutionContext.js
@@ -0,0 +1,20 @@
+// execution context means the environment in which the JavaScript code is executed. 
+// It consists of the variable environment, the scope chain, and the this value. 
+// The execution context is created when a function is called or when the global code is executed. 
+// It determines how variables and functions are accessed and how the this keyword behaves.
+//  Understanding execution context is crucial for understanding how JavaScript works and how to write efficient code.
+
+/*
+
+What Exactly is Execution Context?
+
+👉 Think of it as a box/environment where JS code is executed.
+
+Whenever JavaScript runs:
+
+1.It creates an execution context
+2.Executes code inside it
+3.Then destroys it
+*/
+
+ 
