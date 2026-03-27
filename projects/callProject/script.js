0eb1bcb56d2bdc2b38cbcf23696e8f8fe1087bf6
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-27 11:32:48 +0530

    making changes into the project

--- projects/callProject/script.js
+++ projects/callProject/script.js
@@ -1,5 +1,10 @@
 let add = document.querySelector("#add");
+let close = document.querySelector(".close");
 add.addEventListener("click", function() {
     let formContainer = document.querySelector(".form-container");
     formContainer.style.display = "block";
+});
+close.addEventListener("click", function() {
+    let formContainer = document.querySelector(".form-container");
+    formContainer.style.display = "none";
 });
