9a5e188de4bacfe1f1b696aca255319fd8e807e7
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-27 11:42:03 +0530

    making changes into the project

--- projects/callProject/script.js
+++ projects/callProject/script.js
@@ -1,9 +1,19 @@
 let add = document.querySelector("#add");
 let close = document.querySelector(".close");
    let formContainer = document.querySelector(".form-container");
+   const radios = document.querySelectorAll('input[name="category"]');
 add.addEventListener("click", function() {
     formContainer.style.display = "block";
 });
 close.addEventListener("click", function() {
     formContainer.style.display = "none";
 });
+
+
+  
+
+  radios.forEach(radio => {
+    radio.addEventListener('change', function () {
+      console.log("Selected:", this.value);
+    });
+  });
