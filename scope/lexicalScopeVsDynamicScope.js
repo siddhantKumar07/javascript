c8afca06e0b9f5cf114be48fb55dcb09ddbcad6c
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-27 20:16:12 +0530

    lexical scoping vs dynamic scoping

--- scope/lexicalScopeVsDynamicScope.js
+++ scope/lexicalScopeVsDynamicScope.js
@@ -1,5 +1,7 @@
 // lexical scope means that variables are accessible based on their location in the source code. A variable defined in an outer scope is accessible in an inner scope, but not vice versa.
 
+// in simple words - A variable is searched based on where it was written, not who called the function.
+
 function outerFunction() {
     let outerVariable = "I am in the outer function"; 
     function innerFunction() {
@@ -13,14 +15,6 @@ outerFunction();
 // dynamic scope means that variables are accessible based on the call stack at runtime. A variable defined in a function is accessible to any function that is called after it, regardless of where it is defined in the source code.  
 
 // in simple words -A variable is searched based on who called the function, not where it was written.
-function dynamicScopeExample() {
-    let dynamicVariable = "I am a dynamic variable";
-    function innerFunction() {
-        console.log(dynamicVariable); // This will work because innerFunction is called after dynamicVariable is defined
-    } 
-    innerFunction();
-}
-dynamicScopeExample();
 
 
 let variable = 10;
@@ -32,6 +26,7 @@ let variable = 10;
     let variable =25;
     testDynamicScope();// if it is dynamic scope, it will print 25 because callerFunction is calling testDynamicScope and variable is defined in callerFunction. But if it is lexical scope, it will print 10 because variable is defined in the global scope and testDynamicScope has access to variables in its outer scope.
  }
+callerFunction(); // This will print 10 because JavaScript uses lexical scoping, not dynamic scoping.
 
 //🔴 Important Point
 
