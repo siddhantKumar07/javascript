a2ee5a4652e3911d9dba0465b46589180d936c30
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-27 11:44:33 +0530

    making changes into the project

--- projects/callProject/script.js
+++ projects/callProject/script.js
@@ -2,14 +2,14 @@ let add = document.querySelector("#add");// this is for to select the add button
 let close = document.querySelector(".close");//this is used for to select the close button 
 let formContainer = document.querySelector(".form-container");// this is for to show the form when the add button is clicked and hide it when the close button is clicked
 const radios = document.querySelectorAll('input[name="category"]');//  this is for to check which radio button is selected and log it to the console
-add.addEventListener("click", function () {
+add.addEventListener("click", function () {//when click on add button the form will be shown
   formContainer.style.display = "block";
 });
-close.addEventListener("click", function () {
+close.addEventListener("click", function () {//when click on close button the form will be hidden
   formContainer.style.display = "none";
 });
 
-radios.forEach((radio) => {
+radios.forEach((radio) => {// this is for to check which radio button is selected and log it to the console
   radio.addEventListener("change", function () {
     console.log("Selected:", this.value);
   });
