41006b82c06f3e4e9052b6a0efd4c655fc73872d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-31 23:35:29 +0530

    learning bind

--- thisKeyword/bind.js
+++ thisKeyword/bind.js
@@ -8,9 +8,12 @@ const myObject = {
 };
 const myFunction = myObject.myMethod;
 myFunction(); // This will log undefined because this is not set to myObject
+
 const boundFunction = myFunction.bind(myObject);
 boundFunction(); // This will log "My Object" because this is set to myObject   
+
 // Example of using bind to set the value of this in an event handler
 const button = document.querySelector('button');
 button.addEventListener('click', myObject.myMethod.bind(myObject)); // This will log "My Object" when the button is clicked because this is set to myObject in the event handler
+
 // In summary, bind is a powerful method that allows you to set the value of this for a function and ensure that it is consistent when the function is called in different contexts. It is especially useful in event handlers and when working with callbacks.
