0b41defab5a0c8ba48b07eaac493df4585e602bc
siddhantKumar07 <siddhantk80044@gmail.com>
2026-03-31 20:27:16 +0530

    learning the concept of this keyword

--- thisKeyword/learning.js
+++ thisKeyword/learning.js
@@ -25,7 +25,7 @@ class MyClass {
     }
 }
 const myInstance = new MyClass('My Instance');
-myInstance.myMethod(); // This will log "My Instance"
+myInstance.myMethod(); // This will log "My 
 // In arrow functions, this refers to the surrounding context
 const myArrowFunction = () => { 
     console.log(this);
