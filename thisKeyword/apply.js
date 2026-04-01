a47fd4c7217032df6120eafbb8ac15552eb9ea8a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-01 23:27:53 +0530

    learning apply Function

--- thisKeyword/apply.js
+++ thisKeyword/apply.js
@@ -1,1 +1,2 @@
 // apply is used to call a function with a specific this value and arguments provided as an array (or an array-like object). It is similar to call, but instead of passing arguments individually, we pass them as an array. 
+const myObject = {
