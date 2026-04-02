1b8cebfdca6ae1aed6912e588ba63fb4c4d677aa
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-02 23:29:04 +0530

    learning class

--- thisKeyword/allUse.js
+++ thisKeyword/allUse.js
@@ -5,3 +5,7 @@ name(){
 }
 
 }
+let obj = new name();
+obj.name(); // This will log "This is the use of this keyword in JavaScript"
+
+// In the above code, we have a class named "name" with a method called "name". When we create an instance of the class and call the method, it logs a message to the console. The "this" keyword is used to refer to the current instance of the class, allowing us to access its properties and methods.
