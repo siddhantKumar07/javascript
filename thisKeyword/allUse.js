413410422386d2067aeb87977e7d57dfdb459049
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-02 23:31:45 +0530

    learning class

--- thisKeyword/allUse.js
+++ thisKeyword/allUse.js
@@ -1,5 +1,5 @@
 // a class in JavaScript is a blueprint for creating objects. It defines properties and methods that the objects created from the class will have. The "this" keyword is used within a class to refer to the current instance of the class, allowing us to access its properties and methods.
-// In the above code, we have a class named "name" with a method called "name". When we create an instance of the class and call the method, it logs a message to the console. The "this" keyword is used to refer to the current instance of the class, allowing us to access its properties and methods.
+//
 class name{
 name(){
     console.log("This is the use of this keyword in JavaScript");
