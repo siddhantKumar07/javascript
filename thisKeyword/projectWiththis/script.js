28e9c0455289d5e7b04e6ae1dd15545a6c2cc8cb
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-03 22:47:37 +0530

    creating a project to learn bind() in deep

--- thisKeyword/projectWiththis/script.js
+++ thisKeyword/projectWiththis/script.js
@@ -4,19 +4,27 @@ let roleInput = form.querySelector("input[placeholder='Role']");
 let bioInput = form.querySelector("textarea[placeholder='Bio']");
 let photoInput = form.querySelector("input[placeholder='Photo URL']");
 
+const userManager ={
+    users: [],
 
-form.addEventListener("submit", function(e){
+    init : function (){
+        form.addEventListener("submit", this.submitForm.bind(this));
+    },
+    submitForm :function(e){
     e.preventDefault();
-    let name = nameInput.value;
-    let role = roleInput.value;
-    let bio = bioInput.value;
-    let photo = photoInput.value;
-    let user = {
-        name: name,
-        role: role, 
-        bio: bio,
-        photo: photo
-    }
-    console.log(user);
+    // let name = nameInput.value;
+    // let role = roleInput.value;
+    // let bio = bioInput.value;
+    // let photo = photoInput.value;
+    // let user = {
+    //     name: name,
+    //     role: role, 
+    //     bio: bio,
+    //     photo: photo
+    // }
+    console.log(this);
 }
-);
+}
+
+
+ 
