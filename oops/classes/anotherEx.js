f5fcf7fb993f73ff83a2797f815d65f47799803f
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-04 23:14:52 +0530

    learning with example

--- oops/classes/anotherEx.js
+++ oops/classes/anotherEx.js
@@ -6,8 +6,9 @@ class BiscuitFactory {
      this.flavour=flavour
     }
     details(){
-        console.log(`biscuit name :${this.name} quantity in 1 packet :${this.quantity}`)
+        console.log(`biscuit name :${this.name}, quantity in 1 packet :${this.quantity}, price :${this.price} and  flavour :${this.flavour}`)
     }
 }
 
 const parle = new BiscuitFactory("parleG",8,5,"sweet")
+parle.details();
