51d65747b816f2b79604d6e36b5b3ed9ca4aa658
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-04 20:06:58 +0530

    completing the project with bind()

---
+++ oops/definition.js
@@ -0,0 +1,1 @@
+// oops is a programming paradigm that uses objects and classes to organize code. It allows us to create reusable code and model real-world entities in our programs. In JavaScript, we can create objects using object literals, constructor functions, or ES6 classes. OOP helps us to write cleaner and more maintainable code by encapsulating data and behavior into objects.
