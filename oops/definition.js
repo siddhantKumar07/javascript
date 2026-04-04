4c686b478edc57c3bd4351d7480ad32419c64b5d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-04 20:08:58 +0530

    completing the project with bind()

--- oops/definition.js
+++ oops/definition.js
@@ -10,3 +10,18 @@ const person = {
 };
 
 person.greet(); // Output: Hello, my name is John and I am 30 years old.
+
+// Example of a constructor function
+function Person(name, age) {
+  this.name = name;
+    this.age = age;
+    this.greet = function() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+const person1 = new Person('Alice', 25);
+const person2 = new Person('Bob', 35);  
+
+person1.greet(); // Output: Hello, my name is Alice and I am 25 years old.
+person2.greet(); // Output: Hello, my name is Bob and I am 35 years old.
