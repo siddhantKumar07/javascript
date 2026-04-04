0fdcb5f47c198aba7bd407f6c4daa3c63e68726d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-04 23:10:00 +0530

    learning with example

---
+++ oops/classes/anotherEx.js
@@ -0,0 +1,8 @@
+class BiscuitFactory {
+    constructor(name,quantity,price,flavour){
+     this.name=name;
+     this.quantity=quantity;
+     this.price=price;
+     this.flavour=flavour
+    }
+}
