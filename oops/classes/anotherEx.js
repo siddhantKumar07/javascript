f84441f4db28d5bc595be8d0650bfdfbd91ed6f2
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-07 14:28:52 +0530

    message

--- oops/classes/anotherEx.js
+++ oops/classes/anotherEx.js
@@ -18,9 +18,3 @@ hideandseek.details();// biscuit name :hide and seek, quantity in 1 packet :10, 
 const britannia = new BiscuitFactory("britannia",12,15,"butter")
 britannia.details();
 
-// thats all about today class and object in js, see you in next topic.
-// In the above code we have created a class called BiscuitFactory which has a constructor to initialize the properties of the class and a method called details to display the details of the biscuit. We have created three objects of the class and called the details method to display the details of each biscuit.    
-
-// Note: In JavaScript, we can create a class using the class keyword and define a constructor to initialize the properties of the class. We can also define methods inside the class to perform certain actions. We can create objects of the class using the new keyword and call the methods to perform actions on the objects.
-
-// In the above code, we have created a class called BiscuitFactory which has a constructor to initialize the properties of the class and a method called details to display the details of the biscuit. We have created three objects of the class and called the details method to display the details of each biscuit.
