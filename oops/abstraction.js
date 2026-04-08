a1a467c03a610faadb005e3876d66be05b917481
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-08 20:17:31 +0530

    learning abstraction

--- oops/abstraction.js
+++ oops/abstraction.js
@@ -1,3 +1,10 @@
 // abstraction is the process of hiding the implementation details and showing only functionality to the user.
 
 // example of abstraction in JavaScript
+
+class Car {
+    constructor(brand, model) {
+        this.brand = brand;
+        this.model = model;
+    }
+}
