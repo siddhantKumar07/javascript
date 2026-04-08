6748368974fb94f6b57fdf3c3e7c1b91f5ebb08b
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-08 22:27:30 +0530

    learning abstarction

--- oops/abstraction.js
+++ oops/abstraction.js
@@ -18,5 +18,4 @@ class CarFactory {
 const carFactory = new CarFactory();
 const myCar = carFactory.createCar('Toyota', 'Corolla');
 
-console.log(myCar); // Car { brand: 'Toyota', model: 'Corolla' }
 
