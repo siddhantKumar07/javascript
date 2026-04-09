c03d8f08e4839982a0200bfbb7456e182c301be3
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-09 20:51:08 +0530

    abstraction in js

--- oops/abstraction.js
+++ oops/abstraction.js
@@ -1,6 +1,7 @@
 // abstraction is the process of hiding the implementation details and showing only functionality to the user.
 
 // example of abstraction in JavaScript
+// it does not have abstraction in the same way as other languages, but we can achieve it using classes and methods.
 
 class Car {
     constructor(brand, model) {
@@ -20,3 +21,4 @@ const myCar = carFactory.createCar('Toyota', 'Corolla');
 
 console.log(myCar); // Car { brand: 'Toyota', model: 'Corolla' }
 
+abstra
