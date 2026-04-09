f435e2d5a92614bf0d13a0d10a129818a380f463
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-09 21:03:21 +0530

    learning function constructor

---
+++ oops/functionConstructor2.js
@@ -0,0 +1,12 @@
+function Constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.greet = function() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+      }   
+}
+
+const person1 = new Constructor("Alice", 30);
+const person2 = new Constructor("Bob", 25);
+person1.greet(); // Output: Hello, my name is Alice and I am 30 years old.
+person2.greet(); // Output: Hello, my name is Bob and I am 25 years old.
