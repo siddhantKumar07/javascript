0b47106579789853a731d9f3f507819338dc7ec5
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-11 22:53:04 +0530

    going to learn prototype

--- oops/prototype.js
+++ oops/prototype.js
@@ -5,3 +5,9 @@
 // prototype is a property of a function object that is used to implement inheritance in JavaScript. It is an object that is associated with every function and is used to store properties and methods that can be inherited by instances created from that function.
 
 // When a function is created, it automatically gets a prototype property that points to an object. This object can be used to add properties and methods that will be shared by all instances created from that function.
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+p
