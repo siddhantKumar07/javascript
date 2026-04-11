28ba8f3d5bdee81ee11c5d3fc845b7bbabe56e81
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-11 22:52:49 +0530

    going to learn prototype

--- oops/prototype.js
+++ oops/prototype.js
@@ -1,3 +1,7 @@
+//Prototype in JavaScript is used to share properties and methods between objects, so you don’t have to copy the same code again and again.
+
+// It saves memory and enables inheritance.
+
 // prototype is a property of a function object that is used to implement inheritance in JavaScript. It is an object that is associated with every function and is used to store properties and methods that can be inherited by instances created from that function.
 
 // When a function is created, it automatically gets a prototype property that points to an object. This object can be used to add properties and methods that will be shared by all instances created from that function.
