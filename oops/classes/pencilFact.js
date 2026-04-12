75fe80ef8e4de38ffac0baa0eb2b1aa08f56f761
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-12 13:53:46 +0530

    learninng in deep about the class

---
+++ oops/classes/pencilFact.js
@@ -0,0 +1,8 @@
+class pencil {
+    constructor(color, length,price,brand) {
+        this.color = color;
+        this.length = length;
+        this.price = price;
+        this.brand = brand;
+    }
+}
