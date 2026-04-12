fef5441e39774b8e392d12c9f1afe18a2c3f1280
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-12 14:13:35 +0530

    learninng inheritance in deep

--- oops/inheritance/first.js
+++ oops/inheritance/first.js
@@ -1,7 +1,11 @@
+// Inheritance in JavaScript allows one class to inherit properties and methods from another class. This promotes code reusability and establishes a natural hierarchical relationship between classes. In JavaScript, we can achieve inheritance using the `extends` keyword in ES6 classes.
+
+// In the example below, we have a base class `animal` with properties and methods. The `dog` class extends the `animal` class, inheriting its properties and methods. We can create an instance of the `dog` class and call the inherited methods.
 class animal{
-    constructor(name, age){
+    constructor(name, age ,breed){
         this.name = name;
         this.age = age;
+        this.breed = breed;
     }
     eat(){
         console.log(`${this.name} is eating.`);
@@ -12,7 +16,7 @@ class animal{
 }
 class dog extends animal{
     constructor(name, age, breed){
-        super(name, age);
+        super(name, age, breed);
     }
 }
 const dog1 = new dog("Buddy", 3, "Golden Retriever");   
