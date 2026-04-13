4f3b5193d472633204f1b4287c7378b2b4d77ca5
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-13 20:35:45 +0530

    learning deep into call back

---
+++ synchronusAsynchronus/promises.js
@@ -0,0 +1,27 @@
+// promises are used to handle asynchronous operations in JavaScript. They represent a value that may be available now, in the future, or never. A promise can be in one of three states: pending, fulfilled, or rejected.
+
+//A Promise in JavaScript is an object that represents the result of an asynchronous operation (something that takes time, like API calls, file reading, etc.).
+
+// example of promise
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const data = { id: 1, name: "John Doe" };
+            resolve(data); // This will be called when the data is successfully fetched 
+            // reject("Error fetching data"); // This will be called if there is an error fetching data 
+        }, 2000);
+    });
+
+}
+fetchData()
+
+    .then(data => {
+        console.log("Data fetched:");
+        console.log(`ID: ${data.id}, Name: ${data.name}`);
+    }   )
+    .catch(error => {
+        console.error("Error:", error);
+    }
+    );
+
+    
