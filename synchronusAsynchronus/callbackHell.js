7c3b79284aa02534537f60a737f72c011f61cbb5
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-13 20:16:21 +0530

    learning call back

--- synchronusAsynchronus/callbackHell.js
+++ synchronusAsynchronus/callbackHell.js
@@ -15,3 +15,7 @@ function displayProfile(profile) {
     console.log(`ID: ${profile.id}, Name: ${profile.name}`);
 }
 getProfile(1, displayProfile); // Fetching profile... will be printed after 2 seconds and then Profile fetched: ID: 1, Name: John Doe will be printed immediately after that
+
+// call back hell is a situation where we have multiple nested callbacks, which makes the code difficult to read and maintain. It is also known as "Pyramid of Doom" because of the way the code is structured. It can be avoided by using Promises or async/await syntax.
+
+// example of call back hell
