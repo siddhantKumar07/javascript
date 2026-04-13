9c7f1b30e1923d42dfaef5cbc8da4144e21492bf
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-13 20:14:57 +0530

    learning call back

---
+++ synchronusAsynchronus/callbackHell.js
@@ -0,0 +1,17 @@
+// call back function is a function which is passed as an argument to another function and is executed after some operation is completed. It is used to handle asynchronous operations in JavaScript.
+
+// example of call back function
+
+
+function getProfile(id, callback) {
+    setTimeout(() => {
+        console.log("Fetching profile...");
+        const profile = { id: id, name: "John Doe" };
+        callback(profile);
+    }, 2000);
+}
+function displayProfile(profile) {
+    console.log("Profile fetched:");
+    console.log(`ID: ${profile.id}, Name: ${profile.name}`);
+}
+getProfile(1, displayProfile); // Fetching profile... will be printed after 2 seconds and then Profile fetched: ID: 1, Name: John Doe will be printed immediately after that
