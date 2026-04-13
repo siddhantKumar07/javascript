3c21acdf1cd7925e1d2f1b8c9256d4b0c4e23a11
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-13 19:58:22 +0530

    learning protoyple inheriatnce

---
+++ synchronusAsynchronus/callBack.js
@@ -0,0 +1,3 @@
+// call back function is a function which is passed as an argument to another function and is executed after some operation is completed. It is used to handle asynchronous operations in JavaScript.
+
+// example of call back function
