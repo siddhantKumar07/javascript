405ed23c7881bbd2fe8e7b2c3e864d5ab0c68cc0
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-13 19:49:45 +0530

    learning protoyple inheriatnce

---
+++ synchronusAsynchronus/sync.js
@@ -0,0 +1,4 @@
+// synchronus is a process that happens one after another. It is a blocking process. It means that the next process will not start until the previous process is completed. For example, if we have a function that takes 5 seconds to execute, then the next function will not start until the first function is completed.
+
+// Asynchronus is a process that happens simultaneously. It is a non-blocking process. It means that the next process can start before the previous process is completed. For example, if we have a function that takes 5 seconds to execute, then the next function can start immediately without waiting for the first function to complete.
+
