2ad3daa6f57f3ae23d9e26dedcc2225a91b2d7c3
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-13 20:17:16 +0530

    learning call back

--- synchronusAsynchronus/callbackHell.js
+++ synchronusAsynchronus/callbackHell.js
@@ -19,3 +19,39 @@ getProfile(1, displayProfile); // Fetching profile... will be printed after 2 se
 // call back hell is a situation where we have multiple nested callbacks, which makes the code difficult to read and maintain. It is also known as "Pyramid of Doom" because of the way the code is structured. It can be avoided by using Promises or async/await syntax.
 
 // example of call back hell
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log("Fetching user...");
+        const user = { id: id, name: "John Doe" };
+        callback(user);
+    }, 2000);   
+}
+
+function getPosts(userId, callback) {
+    setTimeout(() => {
+        console.log("Fetching posts...");
+        const posts = [{ id: 1, title: "Post 1" }, { id: 2, title: "Post 2" }];
+        callback(posts);
+    }, 2000);
+}
+function getComments(postId, callback) {
+    setTimeout(() => {
+        console.log("Fetching comments...");
+        const comments = [{ id: 1, text: "Comment 1" }, { id: 2, text: "Comment 2" }];
+        callback(comments);
+    }, 2000);
+}
+getUser(1, (user) => {
+    console.log(`User fetched: ID: ${user.id}, Name: ${user.name}`);    
+    getPosts(user.id, (posts) => {
+        console.log("Posts fetched:");
+        posts.forEach(post => console.log(`ID: ${post.id}, Title: ${post.title}`));
+        getComments(posts[0].id, (comments) => {
+            console.log("Comments fetched:");
+            comments.forEach(comment => console.log(`ID: ${comment.id}, Text: ${comment.text}`));
+        }
+        );
+    });
+});
+
