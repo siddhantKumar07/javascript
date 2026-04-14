834540b53989a19515866894aff26d3ae03c48a2
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-14 20:04:07 +0530

    deep learning

---
+++ synchronusAsynchronus/newPromises.js
@@ -0,0 +1,20 @@
+let prom = new Promise((resolve, reject)=>{
+setTimeout(()=>{
+    let a = 10;
+    let b = 20;
+    let sum = a + b;
+    if(sum === 30){
+        resolve("The sum is correct");
+    }   
+    else{
+        reject("The sum is incorrect");
+    }
+}, 2000);
+})
+prom
+.then((messge)=>{
+    console.log(messge);
+})
+.catch((error)=>{
+    console.error(error);
+});
