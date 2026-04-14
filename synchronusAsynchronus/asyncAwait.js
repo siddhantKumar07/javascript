4402ca65cc3564157bd4f397fd71e757a4d0a240
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-14 21:35:10 +0530

    learning Async and await

--- synchronusAsynchronus/asyncAwait.js
+++ synchronusAsynchronus/asyncAwait.js
@@ -1,4 +1,4 @@
-// async await is a syntactic sugar built on top of promises. It allows you to write asynchronous code in a more synchronous and readable manner. The async keyword is used to declare a function as asynchronous, and the await keyword is used to pause the execution of the function until a promise is resolved or 
+// async await is a syntactic sugar built on top of promises. It allows you to write asynchronous code in a more synchronous and readable manner. The async keyword is used to declare a function as asynchronous, and the await keyword is used to pause the execution of the function until a promise is resolved or rejected.
 let promises = new Promise((resolve, reject) => {
    setTimeout(()=>{
     if((Math.random()*10)<5){
