fb15db2f9812dd083e7b3683b32fe55b91457d32
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-14 21:47:31 +0530

    learning fetch api

--- synchronusAsynchronus/fetchApi.js
+++ synchronusAsynchronus/fetchApi.js
@@ -1,1 +1,10 @@
 // fetch is a built-in JavaScript function that allows you to make network requests to retrieve resources from a server. It returns a Promise that resolves to the Response object representing the response to the request. You can use fetch to get data from APIs, load files, or send data to a server. Here's an example of how to use fetch:
+
+fetch('https://dummyjson.com/users')
+.then((response)=>{
+    console.log(response);
+    return response.json(); // This will parse the response body as JSON and return a promise that resolves to the parsed data
+})
+.then((data)=>{
+    console.log(data);
+})
