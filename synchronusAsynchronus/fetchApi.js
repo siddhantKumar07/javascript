cc2684b24115063df93fba715269ae5572e3ac89
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-14 23:34:40 +0530

    doing some random project to learn the asyc await in deep

--- synchronusAsynchronus/fetchApi.js
+++ synchronusAsynchronus/fetchApi.js
@@ -1,15 +1,30 @@
 // fetch is a built-in JavaScript function that allows you to make network requests to retrieve resources from a server. It returns a Promise that resolves to the Response object representing the response to the request. You can use fetch to get data from APIs, load files, or send data to a server. Here's an example of how to use fetch:
 let pic = document.querySelector("img");
 let name = document.querySelector("h1");
+let card = document.querySelector(".card");
 fetch('https://dummyjson.com/users')
 .then((response)=>{
     // console.log(response);
     return response.json(); // This will parse the response body as JSON and return a promise that resolves to the parsed data
 })
 .then((data)=>{
-    console.log(data.users); // This will log the first name of the first user in the results array
-    pic.src = data.users[5].image; 
-    name.textContent = data.users[5].firstName; 
+    // console.log(data.users); // This will log the first name of the first user in the results array
+    // pic.src = data.users[5].image; 
+    // name.textContent = data.users[5].firstName; 
+
+    data.users.forEach((user)=>{
+        // let div = document.createElement("div");
+        // div.classList.add("card");
+        card.innerHTML += `
+        <img src="${user.image}" >
+        <h1>${user.firstName}</h1>
+        <p>${user.email}</p>
+        <p>${user.phone}</p>
+        <p>${user.age}</p>
+        <p>${user.gender}</p>`
+        // document.body.append(div);
+    });
+
 })
 .catch((data)=>{
     console.log(data);
