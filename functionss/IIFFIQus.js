dd0688fd21abd5d982bb29fb59c8022790e90bf6
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-17 20:19:59 +0530

    doing some qus with iife

--- functionss/IIFFIQus.js
+++ functionss/IIFFIQus.js
@@ -1,3 +1,13 @@
-(()=>{
+let retfun =(()=>{
 console.log("This is an IIFE function using arrow function syntax");
+let balance;
+function setbalance(amount){
+balance = amount;   
+}
+function getbalance(){
+return balance;
+}
+
 })();
+
+console.log(balance); 
