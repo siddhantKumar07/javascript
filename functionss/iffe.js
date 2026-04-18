538605e31d72509baebe3c4ecb4070d7caaa6fe8
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-18 19:22:28 +0530

    doing practice

--- functionss/iffe.js
+++ functionss/iffe.js
@@ -2,8 +2,7 @@
 //iife is a function that is executed immediately after it is defined. It is a design pattern which produces a lexical scope using JavaScript's function scoping. It is also known as a self-invoking function or a self-executing anonymous function.
 (function() {
     console.log('This is an IIFE');
-})(); // Output: This is an IIFE
-
+})();
 // its also called module pattern because it can be used to create a module that encapsulates private variables and functions, and exposes only the necessary parts to the outside world.
 let counter = (function() {
     let count = 0; // private variable
