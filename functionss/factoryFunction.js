53139c921d54727b6512f4b8177e8f6143766a47
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-18 20:12:51 +0530

    doing practice

--- functionss/factoryFunction.js
+++ functionss/factoryFunction.js
@@ -1,22 +1,22 @@
-function factory (name,price){
-    stock =10;
-   return {
+function factory(name, price) {
+  stock = 10;
+  return {
     name,
     price,
-  checkStock(){
-    console.log(`There are ${stock} items in stock`);
-  },
-addStock(qnty){
-    stock += qnty;
-    console.log(`Added ${qnty} items to stock. Total stock: ${stock}`);
-   },
-   buyProduct(qnty){
-    if(qnty > stock){
+    checkStock() {
+      console.log(`There are ${stock} items in stock`);
+    },
+    addStock(qnty) {
+      stock += qnty;
+      console.log(`Added ${qnty} items to stock. Total stock: ${stock}`);
+    },
+    buyProduct(qnty) {
+      if (qnty > stock) {
         console.err(`Not enough stock. Only ${stock} items available.`);
-    } else {
+      } else {
         stock -= qnty;
         console.log(`Purchased ${qnty} items. Remaining stock: ${stock}`);
-    }
-   }
+      }
+    },
+  };
 }
-   }
