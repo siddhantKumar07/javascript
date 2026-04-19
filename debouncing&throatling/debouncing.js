d8e3b9ce280c6816fb1bd6e39c0f89d3425f96b4
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-19 12:51:43 +0530

    learning debouncing

---
+++ debouncing&throatling/debouncing.js
@@ -0,0 +1,1 @@
+// debouncing means that the function will be called after a certain amount of time has passed since the last time it was called. This is useful for things like search input, where you don't want to make a request to the server every time the user types a character.
