32a412d6734eb065245db74301a3f5452cf5ec39
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-19 12:54:04 +0530

    learning debouncing

--- debouncing&throatling/debouncing.js
+++ debouncing&throatling/debouncing.js
@@ -1,1 +1,12 @@
 // debouncing means that the function will be called after a certain amount of time has passed since the last time it was called. This is useful for things like search input, where you don't want to make a request to the server every time the user types a character.
+
+function debounce(func, delay) {
+    let timeoutId;
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }
+        , delay);
+    };
+}
