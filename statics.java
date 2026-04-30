8c0d8b7bd2a2b51b1483d51f806dc86175218b5d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-30 20:56:39 +0530

    doing some qus of java for static block

--- statics.java
+++ statics.java
@@ -1,11 +1,15 @@
-class static {
-
+class statics {
+     static int a = 10;
     static {
         System.out.println("Static Block 1");
     }
 
     static {
         System.out.println("Static Block 2");
+    }
+    static void display() {
+        System.out.println("Value of a: " + a);
+        System.out.println("Static method");
     }
 
     public static void main(String[] args) {
