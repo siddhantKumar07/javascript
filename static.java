8746f366fec0364b7eb8733e7d32547991107c8f
siddhantKumar07 <siddhantk80044@gmail.com>
2026-04-30 20:55:20 +0530

    doing some qus of java for static block

---
+++ static.java
@@ -0,0 +1,14 @@
+class static {
+
+    static {
+        System.out.println("Static Block 1");
+    }
+
+    static {
+        System.out.println("Static Block 2");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+}
