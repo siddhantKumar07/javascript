57800c3e66df99b392b4f29718f45142988183fe
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-03 22:44:09 +0530

    doing practice

--- thro.java
+++ thro.java
@@ -1,6 +1,17 @@
 import java.util.Arrays;
 public class thro {
     public static void main(String[] args) {
-        
+        int arr[]= new int[5];
+        try{
+            System.out.println(arr[5]);
+        }
+        catch(Exception e){
+    e.getMessage();
+    e.printStackTrace();
+        }
+        // catch(Throwable e){
+        //     System.out.println("exception occurs ");
+        // e.printStackTrace();
+        // }
     }
 }
