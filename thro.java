450a61203ee926d058d59d9293c01869186f7ff7
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-04 19:48:50 +0530

    practice

--- thro.java
+++ thro.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 public class thro {
     public static void main(String[] args) {
         int arr[]= new int[5];
-        try{
-            System.out.println(arr[5]);
-        }
+  
       
     }
 }
