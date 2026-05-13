2af4d8a3e679b939b1f80032c004bf63dc659d3a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-13 11:09:19 +0530

    doing some qus

--- functionss/funk.js
+++ functionss/funk.js
@@ -6,3 +6,13 @@ some();
 let a = some;
 console.log("hello");
 a();
+
+let obj={
+    
+    mthod:function(){
+        for(let i=0;i<5;i++){
+            console.log(i);
+        }
+    }
+}
+obj.mthod();
