06ec90e2f4428a75b6a717c0a49fd47a9c47eddd
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-17 11:21:55 +0530

    doing practice of java

--- learningJava/abstrac.java
+++ learningJava/abstrac.java
@@ -1,3 +1,12 @@
+abstract class animal{
+    abstract void sound();
+    String name;
+    animal(String name){
+        this.name = name;
+    }
+    // also can create the default methods in abstatrct class
+}
+
 public class abstrac {
     public static void main(String[] args) {
         System.out.println("This is an abstract class example.");
