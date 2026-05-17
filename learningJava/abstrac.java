ffb3808126285ea211bc82cfa05bed9bc9023c52
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-17 11:22:38 +0530

    doing practice of java

--- learningJava/abstrac.java
+++ learningJava/abstrac.java
@@ -5,6 +5,12 @@ abstract class animal{
         this.name = name;
     }
     // also can create the default methods in abstatrct class
+    void eat() {
+        System.out.println(name + " is eating.");
+    }
+    void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
 }
 
 public class abstrac {
