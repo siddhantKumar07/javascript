cb21658303df5c4bb3e328697cf63086d7807c76
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-18 00:26:55 +0530

    learning generic

--- learningJava/genericMethod.java
+++ learningJava/genericMethod.java
@@ -1,3 +1,13 @@
 public class genericMethod {
-    
+    <T> void display(T data){
+        System.out.println("Data: " + data);
+    }
+    public static void main(String[] args) {
+        genericMethod obj = new genericMethod();
+        genericMethod obj2 = new genericMethod();
+        genericMethod obj3 = new genericMethod();
+        obj.display(12);
+        obj2.display("hell.o");
+        obj3.display(3.55423333);
+    }
 }
