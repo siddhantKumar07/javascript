c65ff5c714fd9dd60e25758315397eecd93a92da
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-19 23:17:39 +0530

    learning js in deep with creating a kanban project

---
+++ projects/kanban/script.js
@@ -0,0 +1,4 @@
+let todo = document.querySelector("#todo");
+let doing = document.querySelector("#progress");
+let done = document.querySelector("#done");
+let tasks = document.querySelectorAll(".task");
