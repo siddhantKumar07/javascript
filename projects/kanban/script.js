182222ccb6d055d7f4015abb7f09285daf6c4de1
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-20 00:00:09 +0530

    learning js in deep with creating a kanban project

--- projects/kanban/script.js
+++ projects/kanban/script.js
@@ -3,15 +3,26 @@ let doing = document.querySelector("#progress");
 let done = document.querySelector("#done");
 let tasks = document.querySelectorAll(".task");
 tasks.forEach((task) => {
-task.addEventListener("drag", (e) => {
-   console.log(e)
-    });
+  task.addEventListener("drag", (e) => {
+    console.log(e);
+  });
+});
+todo.addEventListener("dragenter", (e) => {
+  todo.classList.add("dotted");
+});
+todo.addEventListener("dragleave", (e) => {
+  todo.classList.remove("dotted");
 });
 
-doing.addEventListener("dragenter",(e)=>{
-    doing.classList.add("dotted");
-})
-done.addEventListener("dragenter",(e)=>{
-    done.classList.add("dotted");
-
-})
+doing.addEventListener("dragenter", (e) => {
+  doing.classList.add("dotted");
+});
+doing.addEventListener("dragleave", (e) => {
+  doing.classList.remove("dotted");
+});
+done.addEventListener("dragenter", (e) => {
+  done.classList.add("dotted");
+});
+done.addEventListener("dragleave", (e) => {
+  done.classList.remove("dotted");
+});
