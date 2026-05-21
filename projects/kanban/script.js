43d8e66ad3688ff783b3d6897b7bacdecf34ebc5
siddhantKumar07 <sidddhantk80044@gmail.com>
2026-05-21 23:28:44 +0530

    adding the drag feature

--- projects/kanban/script.js
+++ projects/kanban/script.js
@@ -2,7 +2,28 @@ let todo = document.querySelector("#todo");
 let doing = document.querySelector("#progress");
 let done = document.querySelector("#done");
 let tasks = document.querySelectorAll(".task");
+let container = document.querySelector(".container")
 let dragElement =null;
+let form=document.querySelector("form");
+let submitbtn=document.querySelector("#submitbtn");
+let taskname=document.querySelector("#taskname");
+let description=document.querySelector("#description"); 
+let addNewTaskbtn=document.querySelector("#addNewTaskbtn");
+
+addNewTaskbtn.addEventListener("click",()=>{
+  form.style.display="block";
+})
+submitbtn.addEventListener('click', (e)=>{
+e.preventDefault();
+container.innerHTML+=`<div draggable="true" class="task">
+                <span class="tasktitle">${taskname.value}</span>
+                <span class="taskdesc">${description.value}</span>
+                <button>DELETE</button>
+            </div>`
+})
+
+
+
 tasks.forEach((task) => {
   task.addEventListener("drag", (e) => {
     
