d1a91dcbe27117ac3b08391c95d016955d04ffd2
siddhantKumar07 <sidddhantk80044@gmail.com>
2026-05-23 15:22:54 +0530

    adding blur feature

--- projects/kanban/script.js
+++ projects/kanban/script.js
@@ -14,70 +14,103 @@ let todoCount = document.querySelector("#todoCount");
 let progressCount = document.querySelector("#progressCount");
 let doneCount = document.querySelector("#doneCount");
 
+
+
 addNewTaskbtn.addEventListener("click",()=>{
   form.style.display="block";
    main.style.filter = "blur(5px)";
 
 })
+
 submitbtn.addEventListener('click', (e) => {
+
   if(taskname.value=="" || description.value==""){
     alert("Please fill all the fields");
     return;
   }
+
   main.style.filter = "blur(0px)";
-    e.preventDefault();
 
-    let task = document.createElement("div");
+  e.preventDefault();
 
-    task.classList.add("task");
+  let task = document.createElement("div");
 
-    task.setAttribute("draggable", "true");
+  task.classList.add("task");
 
-    task.innerHTML = `
-        <span class="tasktitle">${taskname.value}</span>
-        <span class="taskdesc">${description.value}</span>
-        <button>DELETE</button>
-    `;
+  task.setAttribute("draggable", "true");
 
-    task.addEventListener("dragstart", () => {
-        dragElement = task;
-    });
+  task.innerHTML = `
+      <span class="tasktitle">${taskname.value}</span>
+      <span class="taskdesc">${description.value}</span>
+      <button class="deletebtn">DELETE</button>
+  `;
 
-    container.append(task);
+  task.addEventListener("dragstart", () => {
+      dragElement = task;
+  });
 
-    form.style.display = "none";
 
-    form.reset();
+  let deletebtn = task.querySelector(".deletebtn");
+
+  deletebtn.addEventListener("click", () => {
+
+    task.remove();
+
+    todoCount.innerText = todo.childElementCount - 2;
+    progressCount.innerText = doing.childElementCount - 1;
+    doneCount.innerText = done.childElementCount - 1;
+
+  });
+
+
+  container.append(task);
+
+  form.style.display = "none";
+
+  form.reset();
+
 });
 
-
 tasks.forEach((task) => {
+
   task.addEventListener("drag", (e) => {
-    
-  dragElement=task;
+
+    dragElement=task;
+
   });
+
 });
 
 function dashedscale(child){
 
   child.addEventListener("dragover", (e) => {
+
       e.preventDefault();
+
       child.classList.add("dotted");
+
   });
 
   child.addEventListener("dragleave", () => {
+
       child.classList.remove("dotted");
+
   });
 
   child.addEventListener("drop", (e) => {
+
       child.classList.remove("dotted");
+
       child.append(dragElement);
-       todoCount.innerText =todo.childElementCount-2;
-       progressCount.innerText =doing.childElementCount-1;
-       doneCount.innerText =done.childElementCount-1;
-  console.log(e);
+
+      todoCount.innerText =todo.childElementCount-2;
+      progressCount.innerText =doing.childElementCount-1;
+      doneCount.innerText =done.childElementCount-1;
+
   });
+
 }
+
 dashedscale(todo);
 dashedscale(doing);
 dashedscale(done);
