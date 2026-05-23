8c5024b672b8b2cccddd70df6cd94e24e13a62bc
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-23 22:59:49 +0530

    adding the range feature

--- projects/imageEditor/script.js
+++ projects/imageEditor/script.js
@@ -1,11 +1,27 @@
 let inputImage = document.getElementById("file-input");
 let previewImage = document.getElementById("preview-image");
-
+let reset =document.querySelector("#reset");
+let download = document.querySelector("#download");
 inputImage.addEventListener("input",(e)=>{
 previewImage.src = URL.createObjectURL(e.target.files[0]);
 inputImage.value = "";
 })
 
+
+// for reset button
+reset.addEventListener("click",()=>{
+    previewImage.src = "";
+});
+
+// for download the image 
+download.addEventListener("click",()=>{
+    if(previewImage.src){
+        let a = document.createElement("a");
+        a.href = previewImage.src;
+        a.download = "image.jpg";
+        a.click();
+    }
+});
 
 
 // previewImage.src = URL.createObjectURL(e.target.files[0]);
