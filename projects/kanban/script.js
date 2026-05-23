37e2c353821ae0b165fec1dbc509b9c329e1f5c8
siddhantKumar07 <sidddhantk80044@gmail.com>
2026-05-23 15:26:57 +0530

    completing the project

--- projects/kanban/script.js
+++ projects/kanban/script.js
@@ -2,29 +2,25 @@ let todo = document.querySelector("#todo");
 let doing = document.querySelector("#progress");
 let done = document.querySelector("#done");
 let tasks = document.querySelectorAll(".task");
-let container = document.querySelector(".container")
-let dragElement =null;
-let form=document.querySelector("form");
-let submitbtn=document.querySelector("#submitbtn");
-let taskname=document.querySelector("#taskname");
-let description=document.querySelector("#description"); 
-let addNewTaskbtn=document.querySelector("#addNewTaskbtn");
-let main=document.querySelector("main");
+let container = document.querySelector(".container");
+let dragElement = null;
+let form = document.querySelector("form");
+let submitbtn = document.querySelector("#submitbtn");
+let taskname = document.querySelector("#taskname");
+let description = document.querySelector("#description");
+let addNewTaskbtn = document.querySelector("#addNewTaskbtn");
+let main = document.querySelector("main");
 let todoCount = document.querySelector("#todoCount");
 let progressCount = document.querySelector("#progressCount");
 let doneCount = document.querySelector("#doneCount");
 
+addNewTaskbtn.addEventListener("click", () => {
+  form.style.display = "block";
+  main.style.filter = "blur(5px)";
+});
 
-
-addNewTaskbtn.addEventListener("click",()=>{
-  form.style.display="block";
-   main.style.filter = "blur(5px)";
-
-})
-
-submitbtn.addEventListener('click', (e) => {
-
-  if(taskname.value=="" || description.value==""){
+submitbtn.addEventListener("click", (e) => {
+  if (taskname.value == "" || description.value == "") {
     alert("Please fill all the fields");
     return;
   }
@@ -46,69 +42,52 @@ submitbtn.addEventListener('click', (e) => {
   `;
 
   task.addEventListener("dragstart", () => {
-      dragElement = task;
+    dragElement = task;
   });
-
 
   let deletebtn = task.querySelector(".deletebtn");
 
   deletebtn.addEventListener("click", () => {
-
     task.remove();
 
     todoCount.innerText = todo.childElementCount - 2;
     progressCount.innerText = doing.childElementCount - 1;
     doneCount.innerText = done.childElementCount - 1;
-
   });
-
 
   container.append(task);
 
   form.style.display = "none";
 
   form.reset();
-
 });
 
 tasks.forEach((task) => {
-
   task.addEventListener("drag", (e) => {
-
-    dragElement=task;
-
+    dragElement = task;
   });
-
 });
 
-function dashedscale(child){
-
+function dashedscale(child) {
   child.addEventListener("dragover", (e) => {
+    e.preventDefault();
 
-      e.preventDefault();
-
-      child.classList.add("dotted");
-
+    child.classList.add("dotted");
   });
 
   child.addEventListener("dragleave", () => {
-
-      child.classList.remove("dotted");
-
+    child.classList.remove("dotted");
   });
 
   child.addEventListener("drop", (e) => {
+    child.classList.remove("dotted");
 
-      child.classList.remove("dotted");
+    child.append(dragElement);
 
-      child.append(dragElement);
-
-      todoCount.innerText =todo.childElementCount-2;
-      progressCount.innerText =doing.childElementCount-1;
-      doneCount.innerText =done.childElementCount-1;
-
+    todoCount.innerText = todo.childElementCount - 2;
+    progressCount.innerText = doing.childElementCount - 1;
+    doneCount.innerText = done.childElementCount - 1;
   });
-
 }
 
 dashedscale(todo);
