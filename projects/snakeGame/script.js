eec60ec2c44f19b1f6449a5092aae8b37e7b7d14
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-25 11:37:39 +0530

    completing the building of ui and adding the time

--- projects/snakeGame/script.js
+++ projects/snakeGame/script.js
@@ -1,5 +1,7 @@
 let currenttime = document.getElementById("currentTime");
+let currentScore = document.getElementById("currentScore");
+let currenntHighScore = document.getElementById("currentHighScore");
 
 //for to retrieve the current date and time
 let currentTime = new Date().toLocaleTimeString();
-console.log(currentTime);
+currenttime.innerHTML = currentTime;
