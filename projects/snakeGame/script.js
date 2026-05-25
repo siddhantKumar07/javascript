3930da526b11197a8088623fd424777a69392ded
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-25 11:50:37 +0530

    completing the building of ui and adding the time

--- projects/snakeGame/script.js
+++ projects/snakeGame/script.js
@@ -1,7 +1,18 @@
 let currenttime = document.getElementById("currentTime");
 let currentScore = document.getElementById("currentScore");
 let currenntHighScore = document.getElementById("currentHighScore");
-
+let gameBooard= document.querySelector(".game-board");
 //for to retrieve the current date and time
 let currentTime = new Date().toLocaleTimeString();
 currenttime.innerHTML = currentTime;
+
+
+// for to create the cells 
+
+let rows = 20;
+let columns = 20;
+for(let i=0; i<rows*columns; i++){
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    gameBooard.appendChild(cell);
+}
