3b99815b7884bca5ff719e9b0cb8ae4582007dc2
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-25 11:29:48 +0530

    building the snake project to build logic

--- projects/snakeGame/script.js
+++ projects/snakeGame/script.js
@@ -1,2 +1,6 @@
 let currenttime = document.getElementById("currentTime");
+
+//for to retrieve the current date and time
+let currentDateTime = new Date();
 let storedDateTime = currentDateTime.toString();
+console.log(storedDateTime);
