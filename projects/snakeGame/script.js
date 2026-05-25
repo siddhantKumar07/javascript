6ec0bf1c1d8e6874e30946e53c8d2ea6a21b6d24
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-25 13:18:37 +0530

    completing the building of ui and adding the time

--- projects/snakeGame/script.js
+++ projects/snakeGame/script.js
@@ -1,56 +1,18 @@
 let currenttime = document.getElementById("currentTime");
 let currentScore = document.getElementById("currentScore");
 let currenntHighScore = document.getElementById("currentHighScore");
-
-let gameBoard = document.querySelector(".game-board");
-
-/* CURRENT TIME */
-
+let gameBooard= document.querySelector(".game-board");
+//for to retrieve the current date and time
 let currentTime = new Date().toLocaleTimeString();
-
 currenttime.innerHTML = currentTime;
 
 
-/* GRID SIZE */
+// for to create the cells 
 
-let rows = 22;
-let columns = 44;
-
-
-/* MOBILE */
-
-if(window.innerWidth <= 480){
-
-    rows = 20;
-    columns = 20;
-}
-
-/* TABLET */
-
-else if(window.innerWidth <= 768){
-
-    rows = 22;
-    columns = 30;
-}
-
-
-/* CREATE GRID */
-
-gameBoard.style.gridTemplateColumns =
-`repeat(${columns}, 1fr)`;
-
-
-gameBoard.style.gridTemplateRows =
-`repeat(${rows}, 1fr)`;
-
-
-/* CREATE CELLS */
-
-for(let i = 0; i < rows * columns; i++){
-
+let rows = 45;
+let columns = 21.5;
+for(let i=0; i<rows*columns; i++){
     let cell = document.createElement("div");
-
     cell.classList.add("cell");
-
-    gameBoard.appendChild(cell);
+    gameBooard.appendChild(cell);
 }
