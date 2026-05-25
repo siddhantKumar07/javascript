b5d35a8b62753127a09566f4d4c6a2780ffa751a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-25 11:32:33 +0530

    adding the time feature

--- projects/snakeGame/script.js
+++ projects/snakeGame/script.js
@@ -2,4 +2,4 @@ let currenttime = document.getElementById("currentTime");
 
 //for to retrieve the current date and time
 let currentTime = new Date().toLocaleTimeString();
-console.log(storedDateTime);
+console.log(currentTime);
