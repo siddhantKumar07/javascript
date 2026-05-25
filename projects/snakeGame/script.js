28e7c3a0261517ffa31f675c41c814eb066730a0
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-25 19:39:21 +0530

    completing the building of ui and adding the time

--- projects/snakeGame/script.js
+++ projects/snakeGame/script.js
@@ -9,26 +9,28 @@ currenttime.innerHTML = currentTime;
 // for to create the cells
 function setupGrid(){
 
-    let columns = Math.floor(window.innerWidth / 50);
+    let columns = Math.floor(window.innerWidth / 72);
 
-    let rows =Math.floor(window.innerHeight / 45);
+    let rows =Math.floor(window.innerHeight / 58);
       console.log(columns,rows)
 
     gameBooard.style.gridTemplateColumns =
-      `repeat(${columns}, 32px)`;
+      `repeat(${columns}, 50px)`;
 
     gameBooard.style.gridTemplateRows =
-      `repeat(${rows}, 32px)`;
+      `repeat(${rows}, 50px)`;
 
     gameBooard.innerHTML = "";
 
-    for(let i = 0; i < rows * columns; i++){
+    for(let i = 0; i < rows; i++){
+        for(let j =0;j<columns ;j++){
 
         let cell = document.createElement("div");
 
         cell.classList.add("cell");
 
         gameBooard.appendChild(cell);
+        }
     }
 }
 
