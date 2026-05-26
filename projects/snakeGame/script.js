398e929cff59dc342a287f6ce8c8953c4403d075
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-26 16:32:08 +0530

    creating the snake

--- projects/snakeGame/script.js
+++ projects/snakeGame/script.js
@@ -3,28 +3,30 @@ let currentScore = document.getElementById("currentScore");
 let currenntHighScore = document.getElementById("currentHighScore");
 let gameBooard= document.querySelector(".game-board");
 let cells = document.querySelectorAll(".cell");
+let columns;
+ let rows;
 //for to retrieve the current date and time
 let currentTime = new Date().toLocaleTimeString();
 currenttime.innerHTML = currentTime;
 
 // blocks to store the each cell as an arrar
 const blocks =[];
-let direction ="down";
+let direction ="ArrowRight";
 
 
 // this is for to take the direction from the input;
-// window.addEventListener("keydown",(e)=>{
-//   if(e.key=="ArrowUp"||e.key=="ArrowDown"||e.key=="ArrowLeft"||e.key=="ArrowRight"){
-//      direction=e.key;
-//   }
-//   console.log(direction)
-// })
+window.addEventListener("keydown",(e)=>{
+  if(e.key=="ArrowUp"||e.key=="ArrowDown"||e.key=="ArrowLeft"||e.key=="ArrowRight"){
+     direction=e.key;
+  }
+  console.log(direction)
+})
 // for to create the cells
 function setupGrid(){
 
-    let columns = Math.floor(window.innerWidth / 72);
+columns = Math.floor(window.innerWidth / 72);
 
-    let rows =Math.floor(window.innerHeight / 58);
+ rows =Math.floor(window.innerHeight / 58);
       console.log(columns,rows)
 
     gameBooard.style.gridTemplateColumns =
@@ -49,6 +51,8 @@ function setupGrid(){
         blocks[`${i}-${j}`]=cell;
         }
     }
+
+    //
 }
 
 setupGrid();
@@ -58,9 +62,7 @@ window.addEventListener("resize", setupGrid);
 //snakes logics 
 
 let snake =[
-  {x:1,y:2},
-  {x:1,y:3},
-  {x:1,y:4}
+  {x:1,y:2}
 ]
 
 function render(){
@@ -69,33 +71,42 @@ function render(){
   })
 }
 
+// gameover logics;
+
+
 //calling the render function every 300 ms;
 
 setInterval(()=>{
  let head = null;
- if(direction=="left"){
+ if(direction=="ArrowLeft"){
  head ={
   x:snake[0].x,
   y:snake[0].y-1
 }
  }
- if(direction=="right"){
-
+ else if(direction=="ArrowRight"){
+head={
+  x:snake[0].x,
+  y:snake[0].y+1
+}
  }
- if(direction=="down"){
+ else if(direction=="ArrowDown"){
   head={
     x:snake[0].x+1,
     y:snake[0].y
   }
  } 
- if(direction=="up"){
+ else if(direction=="ArrowUp"){
 head={
   x:snake[0].x-1,
   y:snake[0].y
 }
  } 
- snake.unshift(head);
-snake.pop()
+ snake.forEach((segment)=>{
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+ })
 
+ snake.unshift(head)
+ snake.pop()
 render()
-},3000)
+},700)
