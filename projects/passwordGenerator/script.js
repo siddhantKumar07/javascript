5c590a970a7787ed40ac1e534b36a19913adc53e
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-27 23:25:33 +0530

    creating a new project which is password generator

--- projects/passwordGenerator/script.js
+++ projects/passwordGenerator/script.js
@@ -1,25 +1,23 @@
 let displayPass = document.querySelector(".display")
 let passRange = document.querySelector("input[data-lengthSlider]");
+let checkBoxes = document.querySelectorAll("input[type='checkbox']");
 
 // for uppercase
 let uppercase =[..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 // Lowercase letters
 const lowercase = [..."abcdefghijklmnopqrstuvwxyz"];
 
-console.log(lowercase);
-
 // Numbers 0-9
 const numbers = [..."0123456789"];
 
-console.log(numbers);
 
 // Symbols
 const symbols = [..."!@#$%^&*()_+-={}[]|:;<>?,./"];
 
-console.log(symbols);
 
 let lengthOfPass =7;
 passRange.addEventListener("input", (e) => {
    lengthOfPass=e.target.value;
 });
 console.log(lengthOfPass)
+
