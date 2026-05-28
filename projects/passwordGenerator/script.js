4fd7e5c28af23fdbedb37f9c26215c0b3bf0c164
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-28 11:05:18 +0530

    adding the shuffle function to shuffle the arrar

--- projects/passwordGenerator/script.js
+++ projects/passwordGenerator/script.js
@@ -1,123 +1,124 @@
-let displayPass = document.querySelector(".display")
+let displayPass = document.querySelector(".display");
 let passRange = document.querySelector("input[data-lengthSlider]");
 let checkBoxes = document.querySelectorAll("input[type='checkbox']");
 let checkedArray;
 let generateBtn = document.querySelector("#passGen");
+let length = document.querySelector("p[data-lengthnumber]");
 
-
-let upperCaseActive=false;
-let lowerCaseActive=true;
-let numberActive=true;
-let symbolActive=false;
+let upperCaseActive = false;
+let lowerCaseActive = true;
+let numberActive = true;
+let symbolActive = false;
 // for uppercase
-let uppercaseArray =[..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+let uppercaseArray = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 // Lowercase letters
 const lowercaseArray = [..."abcdefghijklmnopqrstuvwxyz"];
 
 // Numbers 0-9
 const numberArray = [..."0123456789"];
 
-
 // Symbols
 const symbolsArray = [..."!@#$%^&*()_+-={}[]|:;<>?,./"];
 
-
-let lengthOfPass =8;
+length.innerText = 6;
 passRange.addEventListener("input", (e) => {
-   lengthOfPass=e.target.value;
+  length.innerText = e.target.value;
 });
-console.log(lengthOfPass)
 
-// checkBoxes.addEventListener('input', (e)=>{ // this will work only on single element 
+// checkBoxes.addEventListener('input', (e)=>{ // this will work only on single element
 //     console.log(e)
 // })
 
-function activeCheckboxes(){
-checkBoxes.forEach((check)=>{
-check.addEventListener("input",(e)=>{
-   if(e.target.id=="uppercase"){
-  upperCaseActive=e.target.checked;
-   }
-   else if(e.target.id=="lowercase"){
-  lowerCaseActive=e.target.checked;
-   }
-   else if(e.target.id=="symbol"){
-   symbolActive =e.target.checked;
-   }
-   else if(e.target.id=="number"){
-    numberActive=e.target.checked;
-   }
-})
-
-
-})
+function activeCheckboxes() {
+  checkBoxes.forEach((check) => {
+    check.addEventListener("change", (e) => {
+      if (e.target.id == "uppercase") {
+        upperCaseActive = e.target.checked;
+      } else if (e.target.id == "lowercase") {
+        lowerCaseActive = e.target.checked;
+      } else if (e.target.id == "symbol") {
+        symbolActive = e.target.checked;
+      } else if (e.target.id == "number") {
+        numberActive = e.target.checked;
+      }
+    });
+  });
 }
-    activeCheckboxes()
-
-
-
+activeCheckboxes();
 
 // random password generator
 let totalIndex;
-let generated=[];
-function uppercases(){
-
-    if(upperCaseActive){
-    for(let i=0;i<totalIndex;i++){
-       generated.push(uppercaseArray[Math.floor(Math.random()*uppercaseArray.length)])
-       console.log(uppercaseArray[Math.random()*uppercaseArray.length])
+let generated = [];
+function uppercases() {
+  if (upperCaseActive) {
+    for (let i = 0; i < totalIndex; i++) {
+      generated.push(
+        uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)],
+        console.log(Math.floor(Math.random() * uppercaseArray.length))
+      );
     }
-    }
+  }
 }
-function lowercases(){
-
-    if(lowerCaseActive){
-
-    for(let i=0;i<totalIndex;i++){
-
-       generated.push(lowercaseArray[Math.floor(Math.random()*lowercaseArray.length)])
+function lowercases() {
+  if (lowerCaseActive) {
+    for (let i = 0; i < totalIndex; i++) {
+      generated.push(
+        lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)],
+        console.log(Math.floor(Math.random() * lowercaseArray.length))
+      );
     }
-    }
+  }
 }
-function numbers(){
-
-    if(numberActive){
-    for(let i=0;i<totalIndex;i++){
-       generated.push(numberArray[Math.floor(Math.random()*numberArray.length)])
+function numbers() {
+  if (numberActive) {
+    for (let i = 0; i < totalIndex; i++) {
+      generated.push(
+        numberArray[Math.floor(Math.random() * numberArray.length)],
+        console.log(Math.floor(Math.random() * numberArray.length))
+      );
     }
-    }
+  }
 }
-function symbols(){
-
-    if(symbolActive){
-    for(let i=0;i<totalIndex;i++){
-       generated.push(symbolsArray[Math.floor(Math.random()*symbolsArray.length)])
+function symbols() {
+  if (symbolActive) {
+    for (let i = 0; i < totalIndex; i++) {
+      generated.push(
+        symbolsArray[Math.floor(Math.random() * symbolsArray.length)],
+        console.log(Math.floor(Math.random() * symbolsArray.length))
+      );
     }
-    }
+  }
 }
 
+generateBtn.addEventListener("click", () => {
+  generated = [];
 
-generateBtn.addEventListener("click",()=>{
-    generated=[];
+  checkedArray = [...checkBoxes].filter((box) => {
+    return box.checked; // this will store only the true value
+  }).length;
 
-    checkedArray = [...checkBoxes].filter((box) => {
-    return box.checked;// this will store only the true value
-}).length;
-
-if(checkedArray==0){
-    alert("select at least one box")
+  if (checkedArray == 0) {
+    alert("select at least one box");
     return;
-}
+  }
 
-totalIndex =Math.floor(lengthOfPass/checkedArray);
-     uppercases();
-    lowercases();
-    numbers();
-    symbols()
-    console.log(generated)
-    console.log(upperCaseActive)
-    console.log(lowerCaseActive)
-    console.log(numberActive)
-    console.log(symbolActive)
+  totalIndex = Math.floor(length / checkedArray);
+  uppercases();
+  lowercases();
+  numbers();
+  symbols();
+  console.log(generated);
+  console.log(upperCaseActive);
+  console.log(lowerCaseActive);
+  console.log(numberActive);
+  console.log(symbolActive);
 
-})
+  checkBoxes.forEach((box) => {
+    box.checked = false;
+  });
+
+  upperCaseActive = false;
+  lowerCaseActive = false;
+  numberActive = false;
+  symbolActive = false;
+});
