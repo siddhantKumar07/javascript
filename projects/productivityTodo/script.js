a15cdb2eea45b4b0ac0e91a587bb00e8ef4be49d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-30 21:47:59 +0530

    making the ui

--- projects/productivityTodo/script.js
+++ projects/productivityTodo/script.js
@@ -2,6 +2,8 @@ let elems = document.querySelectorAll(".elem");
 
 elems.forEach((element)=>{
     element.addEventListener("click",(e)=>{
-        
+       console.log(e) 
+       console.log(element)
+        document.querySelectorAll(".fullelem")[element.id].style.display ="block";
     })
 })
