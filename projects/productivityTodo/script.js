270b08baa95d34328a2df4df6b44862d63dce971
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-30 23:33:58 +0530

    making the ui

--- projects/productivityTodo/script.js
+++ projects/productivityTodo/script.js
@@ -1,9 +1,19 @@
 let elems = document.querySelectorAll(".elem");
+let backbtn = document.querySelectorAll(".back");
 
+let active;
 elems.forEach((element)=>{
     element.addEventListener("click",(e)=>{
        console.log(e) 
        console.log(element)
-        document.querySelectorAll(".fullelem")[element.id].style.display ="block";
+       active= document.querySelectorAll(".fullelem")[element.id];
+       active.style.display ="block"
+console.log("active"+active)
+
+    })
+})
+backbtn.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        document.querySelectorAll(".fullelem")[active].style.display ="none";
     })
 })
