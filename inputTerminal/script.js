42c309ee0330f8d9cbf3293f60e91e9a1f7f1a0e
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-06 12:32:44 +0530

    taking input from terminal

--- inputTerminal/script.js
+++ inputTerminal/script.js
@@ -34,6 +34,32 @@
 //     rl.close();
 // });
 
+// let fs = require('fs')
+// let input = fs.readFileSync(0,'utf-8').trim()
+// console.log(input)
+
+// some qus 
 let fs = require('fs')
-let input = fs.readFileSync(0,'utf-8').trim()
-console.log(input)
+let input = fs.readFileSync(0,'utf-8').trim();
+if(input==7){
+    console.log("thala for a reason")
+}
+else if(input=="mahi"){
+    console.log("dhoni  for a reason")
+}
+
+// second method is use to take one input at a time 
+
+let readline = require('readline')
+const r1 = readline.createInterface(
+    {
+        input:process.stdin,
+        output:process.stdout
+    }
+
+)
+r1.question("enter the value :",(ans)=>{
+console.log(ans);
+r1.close()
+})
+
