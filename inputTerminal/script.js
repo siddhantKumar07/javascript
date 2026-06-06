65ef7b13162d71f213b4182d6530f67999be462b
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-06 12:22:35 +0530

    taking input from terminal

---
+++ inputTerminal/script.js
@@ -0,0 +1,35 @@
+// const fs = require('fs');
+// const input = fs.readFileSync(0, 'utf8').trim();
+// console.log("output :-"+input);
+
+// let arr=[]
+// const fs = require('fs');
+// const input = fs.readFileSync(0,'utf-8')
+// for(let i=0;i<5;i++){
+//     arr[i]=input
+// }
+// console.log(arr)
+
+// const fs = require('fs');
+// const arr = fs.readFileSync(0, 'utf8')
+//                 .trim()
+//                 .split(/\r?\n/);
+
+// console.log(arr);
+
+// let mapped = arr.map((elem)=>{
+//     return "hello "+elem;
+// })
+// console.log(mapped)
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Enter a number: ', (input) => {
+//     console.log('You entered:', input);
+//     rl.close();
+// });
